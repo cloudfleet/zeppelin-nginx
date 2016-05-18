7ddf6aeb469c06d7804496e13804dab086c4acc5
Christoph Witzany <christoph@web.crofting.com>
2016-05-18 11:05:05 +0200

    Fix name of HQ domain environmen variable and add real domain

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
-sed -i s/__ZEPPELIN_DOMAIN__/$CLOUDFLEET_DOMAIN/g /etc/nginx/conf.d/default.conf
+sed -i s/__ZEPPELIN_DOMAIN__/${HQ_DOMAIN}/g /etc/nginx/conf.d/default.conf
+sed -i s/__REAL_DOMAIN__/${REAL_DOMAIN}/g /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
